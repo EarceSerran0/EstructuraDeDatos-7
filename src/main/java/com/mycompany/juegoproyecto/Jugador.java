6069b36b6bbaa5473b089eb66665688881e0a246
EarceSerran0 <156737986+EarceSerran0@users.noreply.github.com>
2025-02-27 15:18:17 -0600

    Update Jugador.java
    Funciones jugador (Estas se deben probar primero)

--- src/main/java/com/mycompany/juegoproyecto/Jugador.java
+++ src/main/java/com/mycompany/juegoproyecto/Jugador.java
@@ -1,1 +1,53 @@
+package com.mycompany.juegoproyecto;
+/**
+ *
+ * @author Esteban
+ */
+
+public class Jugador {
+    private String nombre;
+    private int posicion;
+
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+        this.posición = 0; // iniciamos en posición 0
+    }
+
+  
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public int getPosicion() {
+        return posicion;
+    }
+    
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
+
+
+   public void avanzar(int cantidad) {
+        this.posición += cantidad;
+    }
+
+    public void retroceder(int cantidad) {
+        this.posición -= cantidad;
+        if (this.posición < 0) {
+            this.posición = 0; // No permitir posiciones negativas
+        }
+    }
+
+
+
+  
+// Pendiente, prueba
+    //Funcion para controlar posicion del jugador/ 
+    public void irAPosicion(int nuevaPosicion) {
+        this.posicion = Math.max(nuevaPosicion, 0);
+    }
+    
+   
+}
 

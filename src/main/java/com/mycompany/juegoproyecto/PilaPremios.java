08b8093bd3371fa4dddeb3d3f5f2adff1577be8a
Dianamedina2 <157725842+Dianamedina2@users.noreply.github.com>
2025-02-28 17:31:51 -0600

    Update PilaPremios.java

--- src/main/java/com/mycompany/juegoproyecto/PilaPremios.java
+++ src/main/java/com/mycompany/juegoproyecto/PilaPremios.java
@@ -4,10 +4,33 @@
  */
 package com.mycompany.juegoproyecto;
 
+import javax.swing.JOptionPane;
 /**
  *
  * @author Esteban
  */
 public class PilaPremios extends PilaJuego {
-    
+    public PilaPremios() {
+        inicializarPila("Premios");
+    }
+
+    @Override
+    public void listarPila() {
+        Nodo actual = obtenerTopNodo();
+        if (actual == null) {
+            JOptionPane.showMessageDialog(null, "No hay premios disponibles.");
+            return;
+        }
+
+        StringBuilder lista = new StringBuilder("Premios en la pila:\n");
+
+        while (actual != null) {
+            Datos datos = actual.getPiDatos();
+            lista.append(datos.getOperacion()).append(datos.getNumero()).append("\n");
+            actual = actual.getAbajo();
+        }
+
+        JOptionPane.showMessageDialog(null, lista.toString());
+    }
+}
 }

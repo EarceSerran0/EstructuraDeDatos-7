2922008250f75f13a2a169ae8fb6bcfa188141ce
Dianamedina2 <157725842+Dianamedina2@users.noreply.github.com>
2025-02-28 17:12:27 -0600

    Update ColaJugadores.java

--- src/main/java/com/mycompany/juegoproyecto/ColaJugadores.java
+++ src/main/java/com/mycompany/juegoproyecto/ColaJugadores.java
@@ -3,19 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.juegoproyecto;
- import javax.swing.JOptionPane;
+
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Grupo#7
 
  */
-
 public class ColaJugadores {
+    private NodoJugador frente;
+    private NodoJugador fin;
 
+    public ColaJugadores() {
         frente = null;
         fin = null;
     }
+    
+    public boolean estaVacia() {
+    return frente == null;
+}
 
     public void agregarJugador(Jugador jugador) {
         NodoJugador nuevoNodo = new NodoJugador(jugador);
@@ -27,6 +34,18 @@ public class ColaJugadores {
             fin = nuevoNodo;
         }
     }
+    
+    public Jugador quitarJugador() {
+    if (frente != null) {
+        Jugador jugadorQuitado = frente.getJugador();
+        frente = frente.getSiguiente();
+        if (frente == null) {
+            fin = null;
+        }
+        return jugadorQuitado;
+    }
+    return null; // Si la cola está vacía, retorna null
+}
 
     public void listarJugadores() {
         NodoJugador actual = frente;
@@ -36,9 +55,8 @@ public class ColaJugadores {
             actual = actual.getSiguiente();
         }
         JOptionPane.showMessageDialog(null, sb.toString());
-    }  
+    }
 }
-
 
 
 

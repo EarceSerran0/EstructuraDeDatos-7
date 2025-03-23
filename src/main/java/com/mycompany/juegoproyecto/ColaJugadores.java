a6aa520ae8ee8a3e94322d9705141203b191faaa
Dianamedina2 <157725842+Dianamedina2@users.noreply.github.com>
2025-03-22 19:43:42 -0600

    Update ColaJugadores.java

--- src/main/java/com/mycompany/juegoproyecto/ColaJugadores.java
+++ src/main/java/com/mycompany/juegoproyecto/ColaJugadores.java
@@ -4,56 +4,23 @@ package com.mycompany.juegoproyecto;
  *
  * @author Grupo#7
  */
-public class ColaJugadores {
-    private NodoJugador frente;
-    private NodoJugador fin;
-    private int cantidadJugadores;
-
-    public ColaJugadores() {
-        frente = null;
-        fin = null;
-        cantidadJugadores = 0;
-    }
-    
-    public boolean estaVacia() {
-        return frente == null;
-    }
-    
-    public int getCantidadJugadores() {
-        return cantidadJugadores;
-    }
-
+public class ColaJugadores extends Cola {
     public void agregarJugador(Jugador jugador) {
         NodoJugador nuevoNodo = new NodoJugador(jugador);
-        if (frente == null) {
-            frente = nuevoNodo;
-            fin = nuevoNodo;
-        } else {
-            fin.setSiguiente(nuevoNodo);
-            fin = nuevoNodo;
-        }
-        cantidadJugadores++;
+        super.agregar(nuevoNodo);
     }
-    
+
     public Jugador quitarJugador() {
-        if (frente != null) {
-            Jugador jugadorQuitado = frente.getJugador();
-            frente = frente.getSiguiente();
-            if (frente == null) {
-                fin = null;
-            }
-            cantidadJugadores--;
-            return jugadorQuitado;
-        }
-        return null; // Si la cola está vacía, retorna null
+        NodoJugador nodoQuitado = super.quitar();
+        return nodoQuitado != null ? nodoQuitado.getJugador() : null;
     }
-    
+
     public Jugador consultarSiguiente() {
-        if (frente != null) {
-            return frente.getJugador();
-        }
-        return null;
+        NodoJugador nodoFrente = super.consultarFrente();
+        return nodoFrente != null ? nodoFrente.getJugador() : null;
     }
-    
-    
+
+    public int getCantidadJugadores() {
+        return super.getCantidad();
+    }
 }
